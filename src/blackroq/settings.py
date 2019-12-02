9bb26b163b03bdc7d138f142c255750f9dc3661e
thepsalmist <xf.xavierfrank@gmail.com>
2019-12-02 14:12:38 +0300

    updated settings

--- src/blackroq/settings.py
+++ src/blackroq/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "wwh7c5v4=$ws5m-sqzs#hh!s&sbq-unlfy-f+tu%u(jz+=*)9="
+# SECRET_KEY = "wwh7c5v4=$ws5m-sqzs#hh!s&sbq-unlfy-f+tu%u(jz+=*)9="
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blackroqapp.herokuapp.com']
 
 
 # Application definition
